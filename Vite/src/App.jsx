c86138fddcf9f22fb06c19014ad53b4c666ded91
Krystian Florkowski <krystianf06@gmail.com>
2023-03-14 14:53:10 +0100

    Start

--- Vite/src/App.jsx
+++ Vite/src/App.jsx
@@ -1,35 +1,67 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
 import "./App.css";
-import useBearStore from "./store";
-import { shallow } from "zustand/shallow";
+
 
 function App() {
-	// const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0)
 
-	const { orange, violet, imie } = useBearStore(
-		(state) => ({
-			orange: state.orange,
-			violet: state.violet,
-			imie: state.imie,
-		}),
-		shallow
-	);
+	
 
-	const setOrange = useBearStore((state) => state.setOrange);
-	const setViolet = useBearStore((state) => state.setViolet);
-	const setImie = useBearStore((state) => state.setImie);
-	const removeAllBears = useBearStore((state) => state.removeAllBears);
-	const removeText = useBearStore((state) => state.removeText);
-
-	const wyslij = (ev) => {
-		ev.preventDefault();
-		console.log(ev);
-		console.log("test");
-	};
 
 	return (
-		<div className='App'>
+		
+		<div className="App">
+		  <div>
+		    <a href="https://vitejs.dev" target="_blank">
+		      <img src="/vite.svg" className="logo" alt="Vite logo" />
+		    </a>
+		    <a href="https://reactjs.org" target="_blank">
+		      <img src={reactLogo} className="logo react" alt="React logo" />
+		    </a>
+		  </div>
+		  <h1>Vite + React</h1>
+		  <div className="card">
+		    <button onClick={() => setCount((count) => count + 1)}>
+		      count is {count}
+		    </button>
+		    <p>
+		      Edit <code>src/App.jsx</code> and save to test HMR
+		    </p>
+		  </div>
+		  <p className="read-the-docs">
+		    Click on the Vite and React logos to learn more
+		  </p>
+		</div>
+	);
+}
+
+export default App;
+
+
+
+
+// import useBearStore from "./store";
+// import { shallow } from "zustand/shallow";
+
+// const { orange, violet } = useBearStore(
+// 	(state) => ({
+// 		orange: state.orange,
+// 		violet: state.violet,
+// 	}),
+// 	shallow
+// );
+
+// const setOrange = useBearStore((state) => state.setOrange);
+// const setViolet = useBearStore((state) => state.setViolet);
+// const removeAllBears = useBearStore((state) => state.removeAllBears);
+
+
+
+
+
+
+{/* <div className='App'>
 			<h1>Carrots</h1>
 			<div className='card'>
 				<button onClick={setOrange}>orange +</button>
@@ -42,40 +74,5 @@ function App() {
 				<h1>Owner</h1>
 				<h2>Orange: {orange}</h2>
 				<h2>Violet: {violet}</h2>
-				<h2>Imie: {imie}</h2>
 			</div>
-			<form onSubmit={wyslij}>
-				<label>
-					Imie:
-					<input type='text' name='imie' value={imie} onChange={setImie} />
-				</label>
-				<br />
-				<input type='submit' value='Wyślij' />
-			</form>
-		</div>
-		// <div className="App">
-		//   <div>
-		//     <a href="https://vitejs.dev" target="_blank">
-		//       <img src="/vite.svg" className="logo" alt="Vite logo" />
-		//     </a>
-		//     <a href="https://reactjs.org" target="_blank">
-		//       <img src={reactLogo} className="logo react" alt="React logo" />
-		//     </a>
-		//   </div>
-		//   <h1>Vite + React</h1>
-		//   <div className="card">
-		//     <button onClick={() => setCount((count) => count + 1)}>
-		//       count is {count}
-		//     </button>
-		//     <p>
-		//       Edit <code>src/App.jsx</code> and save to test HMR
-		//     </p>
-		//   </div>
-		//   <p className="read-the-docs">
-		//     Click on the Vite and React logos to learn more
-		//   </p>
-		// </div>
-	);
-}
-
-export default App;
+		</div> */}
--- Vite/src/Store.jsx
+++ Vite/src/Store.jsx
@@ -3,12 +3,9 @@ import { create } from 'zustand'
 const useBearStore = create((set) => ({
     orange: 0,
     violet: 0,
-    imie:"",
     setOrange: () => set((state) => ({ orange: state.orange + 1 })),
     setViolet: () => set((state) => ({ violet: state.violet + 1 })),
-    setImie: (ev) => set(() => ({ imie: ev.target.value })),
-    removeAllBears: () => set({ orange: 0, violet: 0, imie: "" }),
-    removeText: () => set({imie:""}),
+    removeAllBears: () => set({ orange: 0, violet: 0 }),
 }))
 
 export default useBearStore;
